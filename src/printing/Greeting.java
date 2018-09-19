f2f574b881d1f68d41e4ac79391ddf99e92db329
samuraixdd <42860848+samuraixdd@users.noreply.github.com>
2018-09-19 14:14:24 +0200

    boi

---
+++ src/printing/Greeting.java
@@ -0,0 +1,13 @@
+package printing;
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		System.out.print(" +\"\"\"\"\"+ \r\n" + 
+				" [|o o|] \r\n" + 
+				"  | ^ | \r\n" + 
+				"  |'-'| \r\n" + 
+				" +-----+");
+				System.out.println(""); 
+	     }
+	}
---
+++ src/printing/flag.java
@@ -0,0 +1,15 @@
+package printing;
+
+public class flag {
+
+	public static void main (String args[]) {
+	       System.out.println ( Twinkle, twinkle, litte star,
+	    		   How I wonder what you are! 
+	    		    Up above the world so high,     
+	    		    Like a diamond in the sky. 
+	    		  Twinkle, twinkle, little star, 
+	    		   How I wonder what you are 
+	 
+	    }
+	
+	}
