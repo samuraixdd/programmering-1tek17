22fb9b04ce6d9424b33375dab27753b4450f26a9
samuraixdd <42860848+samuraixdd@users.noreply.github.com>
2018-10-12 09:22:12 +0200

    input 5

---
+++ src/inputandoutput/input5.java
@@ -0,0 +1,26 @@
+package inputandoutput;
+import java.util.Scanner;
+public class input5 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("input num1: ");
+		int num1 = in.nextInt();
+		
+		System.out.print("input num2: ");
+		int num2 = in.nextInt();
+		
+		System.out.print("Input num3: ");
+		int num3 = in.nextInt();
+		
+		
+		
+		System.out.println(" genomsnitt av 3 nummer är: " +
+		(num1 + num2 + num3) / 3);
+		
+
+	}
+
+}
---
+++ src/inputandoutput/input6.java
@@ -0,0 +1,15 @@
+package inputandoutput;
+
+public class input6 {
+	private static final double Width = 5.6;
+	private static final double Height = 2;
+	public static void main(String[] args) {
+		double width = 5.6;
+		double height = 8.5;
+		       System.out.println("perimeter is = " + width * height);
+		       System.out.println("Area is = " + height * width);
+		
+
+	}
+
+}
